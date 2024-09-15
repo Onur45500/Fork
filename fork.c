4b5a6d914a242f9a7a5a24b16ee269346d9bfe52
Onur AKMESE <onur.akmese@viacesi.fr>
2024-09-15 14:15:18 +0200

    Added the writing of a number in a file named test.txt

--- fork.c
+++ fork.c
@@ -1,2 +1,22 @@
 
 /* fork.c */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+	FILE *fd;
+	int x;
+	
+	x = 5;
+	while(1)
+	{
+		fd = fopen("test.txt", "a");
+		fprintf(fd, "%d\n", x);
+		fclose(fd);
+		sleep(1);
+	}
+
+	return 0;
+}
